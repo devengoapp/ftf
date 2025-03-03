a9afcfd5543a0ad546a0a03e3dab02ec766f7283
Techi Rexach <techi@devengo.com>
2025-03-03 16:46:27 +0100

    First approach

---
+++ spec/fixtures/complementary_sample.rb
@@ -0,0 +1,50 @@
+# Complementary 
+# FTF_B88353412_202501_001.txt
+
+{
+  "fiscal_id": "B88353412",
+  "period": "202501",
+  "request_id": "",
+  "file_type": "C",
+  "products": [
+    # Data register 10 (product) DELETE (its holders and relationships are automatically deleted)
+    {
+      "action": "X",
+      "type": "01",
+      "id_type": "01",
+      "id": "ES8931906516575376987825",
+      "created_at": "20250101",
+      "canceled_at": "20250305"
+    },
+    # Data register 10 (product) DELETE RELATIONSHIP between product and holder
+    {
+      "action": "M",
+      "type": "01",
+      "id_type": "01",
+      "id": "ES8931906516575376987825",
+      "created_at": "20250101",
+      "holders": [
+        {
+          "action": "M",
+          "name": "Aitor Garcia Rey",
+          "id_type": "01",
+          "id": "22754545R", 
+          "country": "ES",
+          "created_at": "19790513",
+          # Data register 30 (relationship product-holder) DELETE
+          "relationships": [
+            {
+              "action": "X",
+              "type": "TIT",
+              "created_at": "20250101",
+              "canceled_at": "20250305"
+            }
+          ]
+        }
+      ]
+    },
+  ],
+  "holders": [
+    # Any person remaining in the FTF not associated with any product will be automatically deleted.
+  ]
+}
---
+++ spec/fixtures/mensual_negative_sample.rb
@@ -0,0 +1,12 @@
+# Negative 
+
+# FTF_B88353412_202501_001.txt
+
+{
+  "fiscal_id": "B88353412",
+  "period": "202501",
+  "request_id": "",
+  "file_type": "N",
+  "products": [],
+  "holders": []
+}
